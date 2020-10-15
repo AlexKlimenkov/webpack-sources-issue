88ebd49c91b90f6e9a419e0deeefdfe03b4b5ea7
Koen Schmeets <hello@koenschmeets.nl>
2020-10-15 16:28:44 +0200

    Initial import

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
+const WrapperPlugin = require('wrapper-webpack-plugin')
+
+module.exports = {
+	entry: path.resolve(__dirname, './index.js'),
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: [
+					{
+						loader: 'ts-loader',
+						options: {
+							transpileOnly: false,
+							projectReferences: true,
+							logLevel: 'info',
+						},
+					},
+				],
+			},
+			// {
+			// 	test: /\.js$/,
+			// 	enforce: 'pre',
+			// 	use: ['source-map-loader'],
+			// },
+		],
+	},
+	// resolve: {
+	// 	extensions: ['.tsx', '.ts', '.js', '.json'],
+	// },
+	devtool: false,
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js',
+	},
+	plugins: [
+		new WrapperPlugin({
+			header: () => fs.readFileSync(path.resolve(__dirname, 'shim.js'), 'utf8'),
+		}),
+	],
+	target: 'web',
+	optimization: {
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					compress: {
+						collapse_vars: false,
+						conditionals: false,
+						comparisons: false,
+					},
+					output: {
+						comments: false,
+					},
+				},
+			}),
+		],
+	},
+}
+
